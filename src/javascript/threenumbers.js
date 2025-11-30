7b957d3edf3313412b6806afba203fb947a11b9b
Alena Lychko <alenaxxprv@gmail.com>
2025-11-30 13:43:06 -0500

    commit

--- src/javascript/threenumbers.js
+++ src/javascript/threenumbers.js
@@ -1,15 +1,17 @@
-function showMaxNumber(){
-    const a = Number(document.getElementById('num1').value);
-    const b = Number(document.getElementById('num2').value);
-    const c = Number(document.getElementById('num3').value);
-    
+const a = document.getElementById('num1');
+const b = document.getElementById('num2');
+const c = document.getElementById('num3');
+const btn = document.getElementById('btn');
+function showMaxNumber() {
+  const num1 = a.value.trim();
+  console.log(num1);
+  //   const result = document.getElementById('result');
+  //   if (!n1 && !n2 && !n3) {
+  //     document.getElementById('result').textContent = '';
+  //     return;
+  //   }
+  //   const max = Math.max(a, b, c);
+  //   result.textContent = 'Найбільше число, яке ви ввели - ' + max;
+}
 
-    if(!document.getElementById("num1").value ||
-!document.getElementById("num2").value ||
-!document.getElementById("num3").value ){
-    result.textContent = 'Найбільше число, яке ви ввели - ?';
-    return;
-}
-let max = Math.max(a,b,c)
-result.textContent = 'Найбільше число, яке ви ввели - ' + max;
-}
+showMaxNumber();
